c04e2f0097f094c5846a3d42ff2d9db00b39d03c
kaveh <kaveh>
2020-11-29 22:45:53 +0000

    .

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,6 @@ run apt-get update &&\
   apt-get install -y vim && \
   apt-get install -y emacs-nox && \
   npm install -g @angular/cli
+run ng new app --interactive=false --style=css --routing=false &&\
+  cd /app &&\
+  npm install --save bootstrap@3
--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 docker build -t angular .
-docker run -d --name ang0 -p4200:4200 -v `pwd`/app:/app angular sh -c "cd /app && ng serve --host 0.0.0.0 --port 4200 --disable-host-check" 
+docker run -d --name ang0 -p4200:4200 -v app:/app angular sh -c "cd /app && ng serve --host 0.0.0.0 --port 4200 --disable-host-check" 
