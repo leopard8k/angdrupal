7b3c5345b1fba5b57b308df780f3634960761c27
kaveh <kaveh>
2020-12-01 22:27:22 +0000

    mid lesson 30

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 docker build -t angular - <Dockerfile
 docker run -d --name ang0 -p4200:4200 -v $(pwd)/app:/app angular sh -c 'sleep `expr 6 \* 3600`' # bring up the server
-docker exec -t ang0 sh -c "cd /app && npm install && npm ci" # install dependencies
-docker exec -t ang0 sh -c "cs /app &&  ng serve --host 0.0.0.0 --port 4200 --disable-host-check" # launch the app
+docker exec ang0 sh -c "cd /app && npm install && npm ci" # install dependencies
+docker exec ang0 sh -c "cd /app &&  ng serve --host 0.0.0.0 --port 4200 --disable-host-check" # launch the app
