065f47da797a636ae8b386a097e57902634df6f5
kaveh <kaveh>
2020-12-01 18:29:53 +0000

    changed to npm ci, instead of install

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 docker build -t angular - <Dockerfile
-docker run -d --name ang0 -p4200:4200 -v $(pwd)/app:/app angular sh -c "cd /app && rm -fr node_modules package-lock.json && ng serve --host 0.0.0.0 --port 4200 --disable-host-check" #bring up the server
+docker run -d --name ang0 -p4200:4200 -v $(pwd)/app:/app angular sh -c "cd /app && npm ci && ng serve --host 0.0.0.0 --port 4200 --disable-host-check" #bring up the server
