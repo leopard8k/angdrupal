71b0733ad9f89d9bd78faccf5de8b6e4a7284e36
kaveh <kaveh>
2020-12-01 18:49:30 +0000

    changed the docker launch to allow for interaction with the running container regardless of state of the launch

--- launch.sh
+++ launch.sh
@@ -1,2 +1,3 @@
 docker build -t angular - <Dockerfile
-docker run -d --name ang0 -p4200:4200 -v $(pwd)/app:/app angular sh -c "cd /app && npm ci && ng serve --host 0.0.0.0 --port 4200 --disable-host-check" #bring up the server
+docker run -d --name ang0 -p4200:4200 -v $(pwd)/app:/app angular sh -c 'sleep `expr 6 i\* 3600`' #bring up the server
+docker exec -it ang0 sh -c "cd /app && npm ci && ng serve --host 0.0.0.0 --port 4200 --disable-host-check" #launch the app
